d18e7f09506f4e8c3d07968d3ed603fff4097f5e
Michał Łoński <michal@lonski.pl>
2023-07-02 22:56:06 +0200

    auth tuning

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,31 @@
-import { Button } from "@mui/material";
-import { logout } from "../../firebase";
+import { Button, CircularProgress } from "@mui/material";
+import { logout, auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Character } from "components/Character/Character";
 
 export const Home = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     logout().then(() => navigate("/"));
   };
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+  }, [loading, navigate, user]);
 
   return (
     <div>
-      Joł
-      <Button onClick={handleLogout}>Logout</Button>
+      {loading && <CircularProgress />}
+      {loading || (
+        <>
+          <Button onClick={handleLogout}>Logout</Button>
+          <Character />
+        </>
+      )}
     </div>
   );
 };
