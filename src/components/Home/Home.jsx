c5651230cc94ae2f6ae5a5531bbe54c6641951de
Michał Łoński <michal@lonski.pl>
2023-07-02 22:11:21 +0200

    auth

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import { Button } from "@mui/material";
 import { logout } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 export const Home = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout().then(() => navigate("/"));
+  };
+
   return (
     <div>
       Joł
-      <Button onClick={logout}>Logout</Button>
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
   );
 };
