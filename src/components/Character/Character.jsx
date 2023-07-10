f69c16101d8bb590ef7dacec7fd135eb6ef26671
Michał Łoński <michal@lonski.pl>
2023-07-10 22:44:22 +0200

    character sheet

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,20 +1,45 @@
+import { Box, LinearProgress, Paper, Typography } from "@mui/material";
 import React from "react";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import WalletIcon from "@mui/icons-material/Wallet";
 
 export const Character = ({ character }) => {
   return (
     <div>
       {character && (
-        <>
-          <div>{character.name}</div>
-          {character.clazz && <div>{character.clazz}</div>}
-          {character.level && <div>Level: {character.level}</div>}
-          {character.current_xp && (
-            <div>
-              XP: {character.current_xp} / {character.next_level_xp}
-            </div>
+        <Paper sx={{ margin: "4px", padding: "4px" }} elevation={2}>
+          <Typography variant="h4">{character.name}</Typography>
+          {character.clazz && (
+            <Typography variant="overline" display="block" gutterBottom>
+              {character.clazz}
+            </Typography>
           )}
-          <div>Złoto: {character.gold}</div>
-        </>
+
+          {character.level && (
+            <>
+              <Box display="flex" justifyContent="left">
+                <TrendingUpIcon />
+                <Typography color={"black"} sx={{ marginLeft: "4px" }}>
+                  Poziom {character.level}
+                </Typography>
+              </Box>
+              <Box>
+                <LinearProgress
+                  variant="determinate"
+                  value={(character.current_xp * 100) / character.next_level_xp}
+                />
+              </Box>
+            </>
+          )}
+          <div>
+            <Box marginTop={"10px"} display="flex" justifyContent="left">
+              <WalletIcon />
+              <Typography color={"black"} sx={{ marginLeft: "4px" }}>
+                {character.gold} zł
+              </Typography>
+            </Box>
+          </div>
+        </Paper>
       )}
     </div>
   );
