366d1fd07e8cb2e807276833b59fc0549d150287
Michal Lonski <michal@lonski.pl>
2026-04-06 23:49:31 +0200

    fix: use vite-tsconfig-paths to handle src/ baseUrl aliases

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      components: fileURLToPath(new URL('./src/components', import.meta.url)),
-      hooks: fileURLToPath(new URL('./src/hooks', import.meta.url)),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.js'],
