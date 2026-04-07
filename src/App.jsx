4d82ef0b6a6ab08d1fdce4d7b44452347a8b6d33
Michal Lonski <michal@lonski.pl>
2026-04-07 08:18:10 +0200

    feat: wire ThemeProvider and set dark RPG background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Box } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./App.css";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -14,12 +17,14 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
-      <Box className="app">
-        <RouterProvider router={router} />
-      </Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box className="app">
+          <RouterProvider router={router} />
+        </Box>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
